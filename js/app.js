1854d6308d18cdd760b0630d5f1b0fd9cb2d3c0a
Janglybits <woodward.chris@gmail.com>
2020-01-23 20:55:21 -0600

    added base js logic

--- js/app.js
+++ js/app.js
@@ -2,3 +2,19 @@
 var nickname = prompt('What is your name?');
 console.log('the user\'s name is ' + nickname);
 alert('Welcome, ' + nickname);
+var questionOne = prompt('Yes or No, I have two cats?');
+var string = questionOne.toUpperCase();
+
+var questionTwo = prompt('Yes or No, this is a placeholder?');
+var string = questionTwo.toUpperCase();
+
+var questionThree = prompt('Yes or No, this is a placeholder?');
+var string = questionThree.toUpperCase();
+
+var questionFour = prompt('Yes or No, this is a placeholder?');
+var string = questionFour.toUpperCase();
+
+var questionFive = prompt('Yes or No, this is a placeholder?');
+var string = questionFive.toUpperCase();
+
+alert('Thanks for visiting my page, ' + nickname);

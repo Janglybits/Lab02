d195fe5421f6014a86e97f9319fc832275852cd4
Janglybits <woodward.chris@gmail.com>
2020-01-29 19:06:48 -0600

    Added base input verification for q1

--- js/app.js
+++ js/app.js
@@ -1,20 +1,26 @@
-'use strict';
+// Get name //
 var nickname = prompt('What is your name?');
 console.log('the user\'s name is ' + nickname);
 alert('Welcome, ' + nickname);
+// Question one //
 var questionOne = prompt('Yes or No, I have two cats?');
-var string = questionOne.toUpperCase();
-
+questionOne = questionOne.toUpperCase();
+if (questionOne === Y || YES) {
+    alert('that\'s correct!');
+} else if (questionOne === N || NO) {
+    alert('Sorry, that is not correct');
+} else alert('Please enter yes or no');
+// Question two //
 var questionTwo = prompt('Yes or No, this is a placeholder?');
-var string = questionTwo.toUpperCase();
-
+questionTwo = questionTwo.toUpperCase();
+// Question three //
 var questionThree = prompt('Yes or No, this is a placeholder?');
-var string = questionThree.toUpperCase();
-
+questionThree = questionThree.toUpperCase();
+// Question four //
 var questionFour = prompt('Yes or No, this is a placeholder?');
-var string = questionFour.toUpperCase();
-
+questionFour = questionFour.toUpperCase();
+//Question five //
 var questionFive = prompt('Yes or No, this is a placeholder?');
-var string = questionFive.toUpperCase();
-
+questionFive = questionFive.toUpperCase();
+// farewell//
 alert('Thanks for visiting my page, ' + nickname);
